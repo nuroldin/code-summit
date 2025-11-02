4c1a7b8b32c6728bf8ce960817a72990ed1d2359
Nuroldin Umpara Pimping <nuroldin.umpara.pimping08@gmail.com>
2025-11-03 01:19:59 +0800

    Update EventCard and ExploreBtn components; adjust image dimensions for better responsiveness and add PostHog integration

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -7,7 +7,7 @@ const ExploreBtn = () => {
     <button type="button" id="explore-btn" className="mt-7 mx-auto" onClick={() => console.log('CLICK')}>
       <a href="#events">
         Explore Events
-        <Image src="/icons/arrow-down.svg" alt="down arrow" width={24} height={24} />
+        <Image src="/icons/arrow-down.svg" alt="down arrow" width={0} height={0} style={{width: "auto", height: "auto"}} />
       </a>
     </button>
   );
